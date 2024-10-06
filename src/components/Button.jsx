91d7cfeeb80cf57f12b2d608218e290c9f11e01c
Bao Ngo <ngobao2001@gmail.com>
2024-10-06 19:22:51 +0800

    add a spinner when button is loading

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,14 @@
-const Button = ({ type = 'button', label, onClick, disabled = false }) => {
+import Spinner from "./Spinner";
+
+const Button = ({ type = 'button', label, onClick, disabled = false, isLoading = false }) => {
   return (
     <button
       type={type}
-      className="space-x-4 bg-teal-700 text-white rounded-md p-2 hover:text-white focus:outline-none focus:text-white focus:bg-teal-600"
+      className="flex space-x-4 bg-teal-700 text-white rounded-md p-2 hover:text-white focus:outline-none focus:text-white focus:bg-teal-600"
       onClick={onClick}
       disabled={disabled}
     >
+      {isLoading && <Spinner />}
       {label}
     </button>
   );
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Spinner() {
+  return (
+    <div className="w-6 h-6 mr-1 border-4 border-gray-300 border-t-4 border-t-blue-500 rounded-full animate-spin"></div>
+  );
+}
